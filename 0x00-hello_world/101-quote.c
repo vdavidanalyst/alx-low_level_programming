15ef6bde7e50a3878e0de9daef105e0aab6102cc
victor <victor.baka16@gmail.com>
2022-09-08 16:33:28 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
- /**
+/**
   * main - prints exactly "and that piece of art is useful"
   * - Dora Korpar, 2015-10-19",
   * followed by a new line, to the standard error.
@@ -9,6 +9,6 @@
   */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
