8d392efbb53e73a63267f89fba602c49b0d2deb5
victor <victor.baka16@gmail.com>
2022-09-11 18:03:09 -0700

    task5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,19 +5,12 @@
  *main - Entry point
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char low, e, q;
-
-	e = 'e';
-	q = 'q';
-
-	for (low = 'a'; low <= 'z'; low++)
-	{
-		if (low != e && low != q)
-			putchar(low);
-	}
-	putchar('\n');
+	int i;
+	
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
 	return (0);
 }
