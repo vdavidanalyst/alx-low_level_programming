eaa09e978bd818856e7dae4b502a97162ec94256
victor <victor.baka16@gmail.com>
2022-09-13 18:16:37 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,15 +19,15 @@ void print_to_98(int n)
 			else if (i == 98)
 				printf("%d\n", i);
 		}
-		else if (n >= 98)
+	}
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
 		{
-			for (j = n; j >= 98; j--)
-			{
-				if (j != 98)
-					printf("%d, ", j);
-				else if (j == 98)
-					printf("%d\n", j);
-			}
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
 }
