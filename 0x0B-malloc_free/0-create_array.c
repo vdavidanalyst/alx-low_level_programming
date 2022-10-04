2e86f8c54ce83f8d3eea8c48c1552443a0363cd9
victor <victor.baka16@gmail.com>
2022-10-04 15:43:52 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  *create_array - creates an array of chars dynamically
- *size: size of the array
+ *@size: size of the array
  *@c: the string
  *Return: 0 means success except defined otherwise
  */
