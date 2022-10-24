6749ea1fdb0165b921731029156c1d49c5ab48b6
victor <victor.baka16@gmail.com>
2022-10-24 13:44:29 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,15 +11,11 @@ size_t print_listint(const listint_t *h)
 {
 	int count = 0;
 
-	if (h != NULL)
+	while (h != NULL)
 	{
-		while (h)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			count++;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
-
 	return (count);
 }
