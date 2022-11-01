0db34778ca6b98f1c8c9a59791a99222cdf636c5
victor <victor.baka16@gmail.com>
2022-10-31 18:10:51 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,30 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
-  * create_file - ...
-  * @filename: ...
-  * @text_content: ...
-  *
-  * Return: ...
-  */
+ * create_file - creates a file and adds permission to it
+ * @filename: The name of the file to be created
+ * @text_content: content to be placed in the created file
+ * Return: 1 if successfull and -1 on err.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	ssize_t fild = 0, chk = 0, i = 0;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	if (!text_content)
+		text_content = "";
+
+	fild = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	if (fild < 0)
 		return (-1);
 
-	if (text_content)
-		write(fd, text_content, _strlen(text_content));
+	while (text_content[i])
+		i++;
+	chk = write(fild, text_content, i);
+	if (chk < 0)
+		return (-1);
 
-	close(fd);
+	close(fild);
 	return (1);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
 }
